c7cd348e04a4745fb74ad7ae7ec094439cda9a96
ahaansekhri <69630587+ahaansekhri@users.noreply.github.com>
2021-04-28 19:21:00 +0800

    Update Food.js

--- Food.js
+++ Food.js
@@ -1,28 +1,17 @@
 class Food{
     constructor(foodStock,lastFed){
       this.image = loadImage("Milk.png")
-
-
-
     }
     
      getFoodStock(){
       foodStock  = database.ref('Food');
       foodStock.on("value",function(data){
          foodS = data.val();
-      })
-    
+      })  
     }
     
      updateFoodStock(x){
-      if(x !== 0){
-        x--;
-      }
-    
-      database.ref('/').update({Food:x});
-    }
-
-    deductFood(){
+      database.ref('/').update({Food:foodS-x, FeedTime:hour()});
 
     }
 
@@ -38,6 +27,4 @@ class Food{
       imageMode(CENTER);
       image(this.image,720,220,70,70);
     }
-    
   }
-
